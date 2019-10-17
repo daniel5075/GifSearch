3f2bfb75ebcd7f9e985d034deff435a79a0e326c
Daniel Groveman <daniel5075@gmail.com>
2019-10-17 08:50:48 -0500

    fixing all buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -31,18 +31,13 @@ function displayGif() {
                 gifImage.attr("data-state", "still");
                 gifDiv.append(gifImage);
                 gifDiv.append(gifRating);
-
                 $("#displayGifs").prepend(gifDiv);
             }
         });
 };
 
 $(document).on("click", ".gif", function () {
-    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
     var state = $(this).attr("data-state");
-    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    // Then, set the image's data-state to animate
-    // Else set src to the data-still value
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
